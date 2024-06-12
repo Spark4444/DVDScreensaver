2b24f2700eb0475b1570608fb289f1c601fd38f6
Spark4444 <tstsilin.youtuba@gmail.com>
2024-06-12 13:48:10 +0200

    Update script.js

--- DVDScreensaver/js/script.js
+++ DVDScreensaver/js/script.js
@@ -1,38 +1,55 @@
-//Varaibles init
+// letaibles init
 const DVD = document.querySelector(".DVD");
 let x = 0;
 let y = 0;
-let xSpeed = 0.25;
-let ySpeed = 0.25;
-var colorArray = ['aqua', 'blue', 'fuchsia', 'gray', 'green', 
-'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red', 
-'silver', 'teal', 'white', 'yellow'];
+// Speed
+let xSpeed = getRandomNumber(0.25,0.75);
+let ySpeed = getRandomNumber(0.25,0.75);
+let colorArray = ["aqua", "blue", "fuchsia", "gray", "green", 
+"lime", "maroon", "navy", "olive", "orange", "purple", "red", 
+"silver", "teal", "white", "yellow"];
 
-//Function that reverses an int
-function reverse(int){
-    int -= int + int;
-    return int;
+// Sets a random first color for the DVD logo
+DVD.style.color = `${GetRandomArray(colorArray)}`;
+
+// Function that reverses a number
+function reverse(num){
+    num -= num + num;
+    return num;
 }
 
-//Animation
+// Chooses a random sign for a number and returns it
+function randomSign(num){
+  if(getRandomNumber(0,1) == 1){
+      return reverse(num);
+  }
+  else{
+    return num;
+  }
+}
+
+xSpeed = randomSign(xSpeed);
+ySpeed = randomSign(ySpeed);
+
+// Animation
 let animation = setInterval(() => {
 
-  if (y <= -24.5) {
+  if (y <= -48.5) {
     ySpeed = reverse(ySpeed);
     DVD.style.color = `${GetRandomArray(colorArray)}`;
   }
 
-  if (y >= 24) {
+  if (y >= 48) {
     ySpeed = reverse(ySpeed);
     DVD.style.color = `${GetRandomArray(colorArray)}`;
   }
   
-  if (x >= 44) {
+  if (x >= 48) {
     xSpeed = reverse(xSpeed);
     DVD.style.color = `${GetRandomArray(colorArray)}`;
   }
 
-  if (x <= -44.5) {
+  if (x <= -48) {
     xSpeed = reverse(xSpeed);
     DVD.style.color = `${GetRandomArray(colorArray)}`;
   }
@@ -40,7 +57,7 @@ let animation = setInterval(() => {
   x += xSpeed;
   y += ySpeed;
 
-  DVD.style.left = `${x}vw`;
-  DVD.style.top = `${y}vw`;
+  DVD.style.left = `${x}%`;
+  DVD.style.top = `${y}%`;
 
 }, 25);
